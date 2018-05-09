ebfea7c95e90f4a30bbbca1c78f4438ff3f794e3
moreal <dev.moreal@gmail.com>
2018-05-09 23:04:34 +0900

    UPDATE: Complete

--- server/app/api/extends/register.py
+++ server/app/api/extends/register.py
@@ -1,4 +1,5 @@
 from flask import request, Blueprint
+from flask import render_template, redirect
 from flask_restful import Api, Resource
 
 api = Api(Blueprint(__name__, __name__, url_prefix='/api/register'))
@@ -16,7 +17,7 @@ class Register(Resource):
         resp = requests.post("http://dms.istruly.sexy/auth", data={"id": id, "pw": pw})
 
         if resp.status_code is not 200:
-            return '', 403
+            return "Wrong Id, Password", 403
 
         class_num = int(request.form['class_num'])
         seat_num = int(request.form['seat_num'])
@@ -34,10 +35,10 @@ class Register(Resource):
         # refresh_token = data['refresh_token']
 
         # Prepare Client for datastore
-        from google.auth import compute_engine
         from google.cloud import datastore
 
-        client = datastore.Client(project="i-like-dsmproject", credentials=compute_engine.Credentials())
+        client = datastore.Client.from_service_account_json(
+            'service_account.json')
 
         # Check duplicate seat
         def isDuplicate(prop):
@@ -45,15 +46,16 @@ class Register(Resource):
             query.add_filter('seat', '=', seat_num)
             query.add_filter('room', '=', class_num)
             query.add_filter(prop, '=', True)
-            return len(list(query.fetch())) is not 0
+            rows = list(query.fetch())
+            return len(list(filter(lambda row: row['id'] != id, rows))) is not 0
 
-        if extend_eleven is "on":
+        if extend_eleven == "on":
             if isDuplicate('extend_eleven'):
-                return "Duplicate Seat", 403
+                return "Duplicated Seat", 403
         
-        if extend_twelve is "on":
+        if extend_twelve == "on":
             if isDuplicate('extend_twelve'):
-                return "Duplicate Seat", 403
+                return "Duplicated Seat", 403
 
         # Upsert
         key = client.key('Students', id, namespace="AutoDMS")
@@ -64,10 +66,10 @@ class Register(Resource):
             "pw": pw,
             "room": class_num,
             "seat": seat_num,
-            "extend_eleven": extend_eleven is "on",
-            "extend_twelve": extend_twelve is "on",
+            "extend_eleven": extend_eleven == "on",
+            "extend_twelve": extend_twelve == "on",
         })
 
         client.put(task)
 
-        return "Good Return", 201
+        return redirect('/', code=302)
