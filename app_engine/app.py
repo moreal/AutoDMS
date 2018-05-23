9bcafc77e4fbf2977ce63fc384f2e85c31dbba08
moreal <dev.moreal@gmail.com>
2018-05-23 10:54:09 +0900

    FIX: Timeout Issue

--- app_engine/app.py
+++ app_engine/app.py
@@ -19,48 +19,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-def randomExtend(id, pw, room, time):
-    from api.dms import applyExtension, getExtensionMaps
-
-    maps = getExtensionMaps(room)
-    
-    for seat in maps:
-        if isinstance(seat, int):
-            res = applyExtension(id, pw, room, seat, time)
-            print(seat, id)
-            if res:
-                return "Success", 200
-
-    return "Failed", 403
-
 @app.route('/auto/dms/extend')
 def extend():
-    from api.dms import applyExtension
-    from google.cloud import datastore as ds
-    
-    client = ds.Client.from_service_account_json("service_account.json")
-    students = list(client.query(namespace="AutoDMS", kind="Students").fetch())
-
-    for student in students:
-        id = student['id']
-        pw = student['pw']
-        room = student['room']
-        seat = student['seat']
-
-        extend_eleven = student['extend_eleven']
-        extend_twelve = student['extend_twelve']
-
-        if extend_eleven:
-            print('eleven', id, room, seat)
-            if not applyExtension(id, pw, room, seat, time=11):
-                randomExtend(id, pw, room, time)
-        
-        if extend_twelve:
-            print('twelve', id, room, seat)
-            if not applyExtension(id, pw, room, seat, time=12):
-                randomExtend(id, pw, room, time)
-
-    return "End", 200
+    import requests
+    resp = requests.post('http://35.231.14.247:5000/api/extend/apply')
+    return resp.text, resp.status_code
     
 @app.errorhandler(500)
 def server_error(e):
